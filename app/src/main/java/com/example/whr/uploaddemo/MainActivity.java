489e4bb03544d5526d131c5ab2d3f420e166875d
cc347504377 <347504377@qq.com>
2017-02-04 15:49:06 +0800

    添加6.0权限

--- app/src/main/java/com/example/whr/uploaddemo/MainActivity.java
+++ app/src/main/java/com/example/whr/uploaddemo/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.whr.uploaddemo;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getPermissionRW(this);
         List<ImageInfo> imageInfos = ImageUtil.getinstance(getContentResolver(), this).getimages();
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
@@ -29,4 +34,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * 申请权限
+     */
+    //声明常量权限
+    private final static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO
+    };
+    public static void getPermissionRW(Activity activity) {
+        // 是否添加权限
+        int permissionW = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissionRecord = ActivityCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO);
+        //如果没有权限,申请权限
+        if (permissionW != PackageManager.PERMISSION_GRANTED||permissionRecord!=PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    activity, PERMISSIONS_STORAGE, 11
+            );
+        }
+    }
 }
